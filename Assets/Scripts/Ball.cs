c172f9c2f05a607eaee63fb878a7ea0920242d7d
madebyskippy <timothys@nyu.edu>
2017-01-21 12:23:51 -0500

    ball generation and regeneration

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	GameObject manager;
+	public int team; //which player the ball is for
+
+	// Use this for initialization
+	void Start () {
+		manager = GameObject.Find ("Manager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void setTeam(int t){
+		team = t;
+		//set sprite color
+		GetComponent<SpriteRenderer>().color = new Color(0.2f*(team+1),0.5f*(team+1),0.5f*(team+1));
+	}
+
+	void OnCollisionEnter2D(Collision2D collider){
+		if (collider.gameObject.tag == "Ground") {
+			Debug.Log("hit stuff");
+			manager.SendMessage ("BallFell", team);
+		}
+	}
+}
