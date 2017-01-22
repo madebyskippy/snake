ff5014b96d57e4f3351cb2ec2dd38d88a92a0c77
Hang <whitewield@126.com>
2017-01-22 12:30:28 -0500

    Particle DONE!

---
+++ Assets/Scripts/CS_KillOnTime.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_KillOnTime : MonoBehaviour {
+	[SerializeField] float myDeathTime = 5;
+	// Use this for initialization
+	void Start () {
+		Destroy (this.gameObject, myDeathTime);
+	}
+}
