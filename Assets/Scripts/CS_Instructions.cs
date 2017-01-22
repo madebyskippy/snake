845d36d579cfffc4c30c0cd6d0e0b530f708d98f
Hang <whitewield@126.com>
2017-01-22 15:47:39 -0500

    instruction fixed

--- Assets/Scripts/CS_Instructions.cs
+++ Assets/Scripts/CS_Instructions.cs
@@ -12,14 +12,15 @@ public class CS_Instructions : MonoBehaviour {
 	void Update()
 	{
 		string[] names = Input.GetJoystickNames();
+
+		Xbox_360_Controller = false;
 		for (int x = 0; x < names.Length; x++)
 		{
 			Debug.Log (names [x].Length);
 			if (names[x].Length == 55) {
 				Debug.Log ("XBOX 360 CONTROLLER IS CONNECTED");
 				Xbox_360_Controller = true;
-			} else 
-				Xbox_360_Controller = false;
+			}
 		}
 
 
