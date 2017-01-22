095aa70a058615364f866c069e710f28c4c1a7c4
Laurenz <laurenzriklin@gmx.at>
2017-01-22 14:34:17 -0500

    sounds sounds sounds

--- Assets/Scripts/CS_Controller.cs
+++ Assets/Scripts/CS_Controller.cs
@@ -6,14 +6,17 @@ public class CS_Controller : MonoBehaviour {
 	[SerializeField] CS_Snake mySnake;
 	[SerializeField] int myPlayerNumber;
 	[SerializeField] GameObject mySelection;
+	[SerializeField] AudioClip sideSound;
 	private string myControllerSuffix = "";
 	private int myStep;
+	private bool justPlayedSideSound;
 
 	[SerializeField] float mySpeed = 0.02f; //How many time does it take to move to the next point
 	private float myAccumulation = 0;
 
 	// Use this for initialization
 	void Start () {
+		justPlayedSideSound = false;
 		myStep = 0;
 		mySelection = Instantiate (mySelection, this.transform);
 
@@ -33,6 +36,8 @@ public class CS_Controller : MonoBehaviour {
 	void Update () {
 //		Debug.Log ("Horizontal" + myControllerSuffix);
 
+		Input.GetAxisRaw ("Horizontal" + myControllerSuffix);
+
 		myAccumulation += Input.GetAxis ("Horizontal" + myControllerSuffix) * Time.deltaTime;
 //		Debug.Log (myAccumulation);
 
@@ -40,9 +45,19 @@ public class CS_Controller : MonoBehaviour {
 			if (myAccumulation > mySpeed) {
 				myAccumulation -= mySpeed;
 				MoveRight ();
+				if (justPlayedSideSound == false) {
+					justPlayedSideSound = true;
+					CS_AudioManager.Instance.PlaySFX (sideSound,0.1f,1 + (float)myStep*0.05f);
+					Invoke ("ResetJustPlayed", 0.15f);
+				}
 			} else if (myAccumulation < mySpeed * -1) {
 				myAccumulation += mySpeed;
 				MoveLeft ();
+				if (justPlayedSideSound == false) {
+					justPlayedSideSound = true;
+					CS_AudioManager.Instance.PlaySFX (sideSound,0.1f,1 + (float)myStep*0.05f);
+					Invoke ("ResetJustPlayed", 0.15f);
+				}
 			}
 		}
 
@@ -69,5 +84,9 @@ public class CS_Controller : MonoBehaviour {
 		myStep--;
 		if(myStep < 0)
 			myStep = 0;
+	}
+
+	void ResetJustPlayed () {
+		justPlayedSideSound = false;
 	}
 }
